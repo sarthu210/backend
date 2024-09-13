c68f0d0f17a708c27ac3217aff01981570ba7056
Sarthak Nande <94531691+sarthu210@users.noreply.github.com>
2024-09-13 17:53:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 1,
     sameSite: 'None', // Ensure SameSite is set to None
-    secure: 'development' === 'production',
+    secure: true,
     httpOnly: true,
   }
 }));
